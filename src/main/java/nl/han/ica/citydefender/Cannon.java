075f923f61e22d03a430ae3581855dcf542ef7ec
Wouter Hummelink <wouter.hummelink@ordina.nl>
2018-04-29 14:53:08 +0200

    Implement WeaponGenerators, Pointvalue, CannonShells

--- src/main/java/nl/han/ica/citydefender/Cannon.java
+++ src/main/java/nl/han/ica/citydefender/Cannon.java
@@ -2,17 +2,23 @@ package nl.han.ica.citydefender;
 
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
+import processing.core.PApplet;
 import processing.core.PGraphics;
 
 public class Cannon extends SpriteObject {
 
-	private static final float ROTATE_INCREMENT = PI/180;
+	private static final float ROTATE_INCREMENT = Settings.CANNON_ROTATE_INCREMENT;
 	private boolean firing;
 	private int direction;
-	private float angle = PI/6;
+	private float angle = 0;
 	private Sprite cannonSprite;
 	private WeaponGenerator gun;
 	
+	/**
+	 * Constructor
+	 * @param x X-position in the world
+	 * @param y Y-position in the world
+	 */
 	public Cannon(float x, float y) {
 		super(new Sprite("resources/media/PNG/Parts/engine4.png"));
 		this.cannonSprite = new Sprite("resources/media/PNG/Parts/gun08.png");
@@ -20,23 +26,28 @@ public class Cannon extends SpriteObject {
 		this.height = this.getImage().height + this.cannonSprite.getHeight();
 		this.x = (CityDefenderGame.GAMEWIDTH/2) - width/2;
 		this.y = (CityDefenderGame.GAMEHEIGHT) - height;
+		this.gun = new CannonShellGenerator(this);
 	}
 
+	/**
+	 * Updates the cannon angle and controls the CannonShellGenerator
+	 */
 	@Override
 	public void update() {
-		if (this.direction == LEFT) {
-			this.angle += ROTATE_INCREMENT;
-		} else if (this.direction == RIGHT) {
+		if (this.direction == LEFT && angle > -80  ) {
 			this.angle -= ROTATE_INCREMENT;
+		} else if (this.direction == RIGHT && angle < 80) {
+			this.angle += ROTATE_INCREMENT;
 		}
-		if (firing) {
-			
+		
+		if (firing) {	
 			gun.generateObject();
 		}
-		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Draws the cannon
+	 */
 	@Override
 	public void draw(PGraphics g) {
 		try {
@@ -44,12 +55,8 @@ public class Cannon extends SpriteObject {
 			g.fill(66, 33, 44);
 			g.image(this.getImage(),x,y+40);
 			g.pushMatrix();
-			
-			
-			
-			
 			g.translate(getCenterX(), getCenterY());
-			g.rotate(angle);
+			g.rotate(PApplet.radians(angle));
 			//g.rect(x, y, 10, 40);
 			g.image(cannonSprite.getPImage(),-cannonSprite.getWidth()/2,(-cannonSprite.getHeight()/2)-20);
 			g.popMatrix();
@@ -58,6 +65,9 @@ public class Cannon extends SpriteObject {
 		}
 	}
 	
+	/**
+	 * Determines if keys that control the cannon are pressed
+	 */
 	@Override
 	public void keyPressed(int keyCode, char character) {
 		System.out.println("KeyPressed");
@@ -78,6 +88,9 @@ public class Cannon extends SpriteObject {
 		}
 	}
 	
+	/**
+	 * Determines of keys that control the cannon are released
+	 */
 	@Override
 	public void keyReleased(int keyCode, char character) {
 		switch (keyCode) {
@@ -91,4 +104,12 @@ public class Cannon extends SpriteObject {
 		}
 	}
 
+	/**
+	 * Returns the direction of the cannon barrel
+	 * @return current cannon angle
+	 */
+	public float getCannonDirection() {
+		// TODO Auto-generated method stub
+		return angle;
+	}
 }
--- src/main/java/nl/han/ica/citydefender/CityDefenderGame.java
+++ src/main/java/nl/han/ica/citydefender/CityDefenderGame.java
@@ -10,35 +10,38 @@ import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
 import nl.han.ica.OOPDProcessingEngineHAN.View.View;
 import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PImage;
 
 public class CityDefenderGame extends GameEngine {
 	public static final int GAMEWIDTH=1280;
 	public static final int GAMEHEIGHT=720;
+	private static CityDefenderGame instance;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	public Vector<IGameObjectGenerator> gameObjectGenerators = new Vector<>();
+	private int score;
 
 	public static void main(String[] args) {
 		PApplet.main(new String[]{CityDefenderGame.class.getName()});
 	}
 
+	public CityDefenderGame( ) {
+		this.instance = this;
+	}
+	
 	@Override
 	public void setupGame() {
+		
 		Logger logger = LogFactory.getLogger();
 		logger.addLogHandler(new FileLogHandler());
-		int worldWidth = 1204;
-		int worldHeight = 903;
+		
 		// TODO Auto-generated method stub
 		this.addGameObject(new TitlePopup(this));
 		
-		SpriteObject s = new SpaceShip(new Sprite("resources/media/PNG/Enemies/enemyBlack5.png"));
-			
-		s.setX(0);
-		s.setY(100);
-		s.setxSpeed(5);
-		this.addGameObject(s);
+		this.gameObjectGenerators.add(new SpaceShipGenerator());
 		
 		Cannon cannon = new Cannon(GAMEWIDTH/2, GAMEHEIGHT);
 		this.addGameObject(cannon);
@@ -46,13 +49,37 @@ public class CityDefenderGame extends GameEngine {
 
 
 	}
+	
+	private PImage createBackground() {
+		PGraphics renderer = createGraphics(GAMEWIDTH, GAMEHEIGHT);
+		renderer.beginDraw();
+		Sprite s = new Sprite("resources/media/Backgrounds/darkPurple.png");
+		PImage p = s.getPImage();
+		int countX = (int) Math.ceil(GAMEWIDTH/(float)p.width);
+		int countY = (int) Math.ceil(GAMEHEIGHT/(float)p.height);
+		for(int x = 0; x<countX; x++) {
+			for(int y = 0; y<countY; y++) {
+				//renderer.background(255);
+				System.out.println(x);
+				renderer.image(p, x*p.width, y*p.height);
+			}
+		}
+		renderer.endDraw();
+		return renderer.get();
+	}
+	
 
 	private void createView(int worldWidth, int worldHeight) {
+		
 		View view = new View(worldWidth, worldHeight);
 		setView(view);
-		view.setBackground(28, 0, 69);
-		size(worldWidth, worldHeight);
 		
+		//view.setBackground(28, 0, 69);
+		PImage p = createBackground();
+		//System.out.println("Image Size: "+p.width+"x"+p.height);
+		//System.out.println("View  Size: "+view.getWorldWidth()+"x"+view.getWorldHeight());
+		view.setBackground(p);
+		size(GAMEWIDTH, GAMEHEIGHT);
 	}
 
 	@Override
@@ -62,5 +89,13 @@ public class CityDefenderGame extends GameEngine {
 			generator.generateObject();
 		}
 
+	}
+	
+	public static CityDefenderGame instance() {
+		return CityDefenderGame.instance;
+	}
+
+	public void addPoints(int pointValue) {
+		this.score += pointValue;
 	}
 }
